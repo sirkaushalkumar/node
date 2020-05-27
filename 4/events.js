7e0312fe40e21704c4f4dd4e779ed86922d0ad74
Kaushal Kumar <sirkaushalkumar@gmail.com>
2020-05-27 07:05:55 -0700

    add module 4

---
+++ 4/events.js
@@ -0,0 +1,15 @@
+const events = require("events");
+const emitter = new events.EventEmitter()
+emitter.on("customEvent", (message, user) => {
+    console.log(`${user}: ${message}`)
+})
+emitter.emit("customEvent", "Hello World", "Computer")
+emitter.emit("customEvent", "That's pretty cool huh?", "Kumar")
+process.stdin.on("data", data => {
+    const input = data.toString().trim();
+    if (input === "exit") {
+        emitter.emit("customEvent", "Goodbye", "process")
+        process.exit();
+    }
+    emitter.emit("customEvent", input, "terminal")
+})
